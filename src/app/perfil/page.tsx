c7ee6569b79751fd1beafe3bcb94e57db24c94b2
Luis Arturo <arturomezsanchez@gmail.com>
2025-06-20 06:10:42 -0600

    [FIX] Usar CarruselAnuncios en src/app/perfil/Page

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -4,7 +4,6 @@ import Navbar from '@/components/Navbar';
 import PerfilForm from './components/PerfilForm';
 import CambiarPasswordForm from './components/CambiarPasswordForm';
 import PerfilInfo from './components/PerfilInfo';
-import CarruselAnuncios from '../components/CarruselAnuncios';
 
 export default function PerfilPage() {
   return (
